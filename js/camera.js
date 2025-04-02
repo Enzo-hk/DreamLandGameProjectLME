8c93fe420531ed8162d6ec0668c3fe2fa2465a39
Leo Migny <pipo.magnus@gmail.com>
2025-04-02 14:02:20 +0200

    issue1: caméra qui bouge

--- js/camera.js
+++ js/camera.js
@@ -1,3 +1,4 @@
+
 export function createCamera(canvas, scene) {
     const camera = new BABYLON.ArcRotateCamera("camera", -Math.PI / 2, Math.PI / 2.5, 3, new BABYLON.Vector3(0, 0, 0));
     camera.attachControl(canvas, true);
@@ -6,4 +7,51 @@ export function createCamera(canvas, scene) {
     scene.activeCamera.keysLeft = [];
     scene.activeCamera.keysRight = [];
     return camera;
+}
+
+export function createFPSCamera(canvas, scene, engine) {
+    scene.useRightHandedSystem = true;
+
+    const camera = new BABYLON.FreeCamera("FPS", new BABYLON.Vector3(0, 1.8, 0), scene);
+    camera.attachControl(canvas, true);
+    camera.rotation = new BABYLON.Vector3.Zero();
+
+    camera.speed = 2.5;
+    camera.inertia = 0;
+    camera.angularSensibility = 500;  // modifiable dans les paramètres
+
+    camera.keysUp = [90];  // modifiable dans les paramètres en fonction du layout
+    camera.keysDown = [83];
+    camera.keysLeft = [81]; 
+    camera.keysRight = [68];
+
+    scene.activeCamera = camera;
+
+    // pointer lock
+    canvas.addEventListener("click", () => {
+        canvas.requestPointerLock();
+    });
+
+    enablePointerLock(true, canvas, camera, engine);
+}
+
+function mouseMove(e, camera, engine) {
+    let deltaTime = engine.getDeltaTime();
+
+    camera.rotation.x += e.movementY * deltaTime * 0.001;
+    camera.rotation.y -= e.movementX * deltaTime * 0.001;
+}
+
+export function enablePointerLock(on, canvas, camera, engine) {
+    if (on) {
+        document.addEventListener("pointerlockchange", () => {  // le bouger pour le modifier quand on est dans un menu
+            if (document.pointerLockElement === canvas) {
+                document.addEventListener("mousemove", mouseMove, camera, engine);
+            } else {
+                document.removeEventListener("mousemove", mouseMove, camera, engine);
+            }
+        });
+    } else {
+        document.removeEventListener("mousemove", mouseMove, camera, engine);
+    }
 }
--- js/scene.js
+++ js/scene.js
@@ -1,4 +1,4 @@
-import { createCamera } from "./camera.js";
+import { createCamera, createFPSCamera } from "./camera.js";
 import { createLight } from "./light.js";
 import { createBox, createGround } from "./objects.js";
 import { setupAnimation } from "./animation.js";
@@ -7,19 +7,19 @@ import { initAudio } from "./sound.js";
 
 export function createScene(engine, canvas) {
     const scene = new BABYLON.Scene(engine);
-    const camera = createCamera(canvas, scene);
+    const camera = createFPSCamera(canvas, scene, engine);
     const light = createLight(scene);
     const box1 = createBox(scene, new BABYLON.Vector3(1, 2, 1), new BABYLON.Vector3(0, 1, 0));
     const box2 = createBox(scene, new BABYLON.Vector3(1, 2, 1), new BABYLON.Vector3(-2, 1, 0));
     const box3 = createBox(scene, new BABYLON.Vector3(1, 2, 1), new BABYLON.Vector3(2, 1, 0));
     const ground = createGround(scene);
     //initAudio();
-    setupAnimation(box1);
+    /**setupAnimation(box1);
     setupAnimation(box2);
     setupAnimation(box3);
     setupControls(scene, box1);
     setupControls(scene, box2);
-    setupControls(scene, box3);
+    setupControls(scene, box3);*/
 
     let y = 3
     window.addEventListener("keypress", (e) => { 
